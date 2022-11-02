1a0b80aabaafbfe9b2ff07201c8601d57f3c4b50
samoanbiscuit <87005043+samoanbiscuit@users.noreply.github.com>
2022-11-03 00:26:15 +1030

    Saving before major change in case of reversion.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GeoFenceApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GeoFenceApp extends StatelessWidget {
+  const GeoFenceApp({super.key});
 
   // This widget is the root of your application.
   @override
